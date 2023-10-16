92886e94a72cad3e3f7a2a4c3513f3398ef32799
leedong617 <leedong617@gmail.com>
2023-10-16 16:48:20 +0900

    ..

--- src/main/java/com/danaga/entity/Member.java
+++ src/main/java/com/danaga/entity/Member.java
@@ -1,5 +1,23 @@
 package com.danaga.entity;
 
-public class Member {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+	@Id
+	private String memberEmail;
+	private String memberId;
+	private String memberPassword;
+	private String memberAddress;
+	private String memberBirthday;
+	private String memberPhoneNo;
 }
