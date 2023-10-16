3d2128ae6bbbc81f498e583a4bdc3f7e2b6eeff2
yukang <choliea91@gmail.com>
2023-10-16 18:25:08 +0900

    d

--- src/main/java/com/danaga/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.danaga.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.danaga.entity.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-}
