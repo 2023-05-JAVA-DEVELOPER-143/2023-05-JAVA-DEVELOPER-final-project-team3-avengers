764f2f71fce204f57280aee0b041cdbe658cd0ba
kkr95101 <kkr95101@naver.com>
2023-10-18 14:23:35 +0900

    orderDaoImpl modify

--- src/main/java/com/danaga/dao/OrderDaoImpl.java
+++ src/main/java/com/danaga/dao/OrderDaoImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.danaga.entity.Delivery;
 import com.danaga.entity.Member;
 import com.danaga.entity.OptionSet;
 import com.danaga.entity.OrderItem;
@@ -23,6 +24,7 @@ public class OrderDaoImpl implements OrderDao {
 	@Autowired
 	MemberRepository memberRepository;
 	
+	
 	@Override
 	public Orders insert(Orders order) {
 		Orders insertOrder = orderRepository.save(order);
@@ -37,11 +39,16 @@ public class OrderDaoImpl implements OrderDao {
 		return insertOrder; // 저장된 주문 객체 반환
 	}
 
+	public Orders insertGuest(Orders order) {
+		return null;
+	}
+	
+	
 	@Override
 	public Orders updateOrderByOState(String oState) {
-		// Orders 테이블에서 oState를 기반으로 주문을 업데이트하려면 적절한 로직을 추가하세요.
-		// orderRepository를 사용하여 업데이트할 주문을 찾아 업데이트하는 방식으로 구현 가능합니다.
-		// orderRepository의 save 또는 update 메서드를 사용하여 주문 정보를 업데이트할 수 있습니다.
+		// Orders 테이블에서 oState를 기반으로 주문을 업데이트하려면 적절한 로직을 추가
+		// orderRepository를 사용하여 업데이트할 주문을 찾아 업데이트하는 방식으로 구현 가능
+		// orderRepository의 save 또는 update 메서드를 사용하여 주문 정보를 업데이트
 		return null;
 	}
 
@@ -56,22 +63,22 @@ public class OrderDaoImpl implements OrderDao {
 
 	@Override
 	public List<Orders> findOrderWithOrderItemByMemberId(Long memberId) {
-		// 주어진 memberId를 사용하여 해당 회원의 주문 목록을 조회하고, 주문 항목을 함께 로드하려면 적절한 로직을 추가하세요.
-		// orderRepository와 JOIN 또는 FetchType.LAZY를 사용하여 관련된 주문 항목을 함께 로드할 수 있습니다.
+		// 주어진 memberId를 사용하여 해당 회원의 주문 목록을 조회하고, 주문 항목을 함께 로드하려면 적절한 로직을 추가
+		// orderRepository와 JOIN 또는 FetchType.LAZY를 사용하여 관련된 주문 항목을 함께 로드
 		return null;
 	}
 
 	@Override
 	public Orders findOrderByOrderNo(Long oNo) {
-		// 주어진 주문 번호 (oNo)를 사용하여 해당 주문을 조회하려면 적절한 로직을 추가하세요.
-		// orderRepository를 사용하여 데이터베이스에서 주문을 조회할 수 있습니다.
+		// 주어진 주문 번호 (oNo)를 사용하여 해당 주문을 조회하려면 적절한 로직을 추가
+		// orderRepository를 사용하여 데이터베이스에서 주문을 조회
 		return orderRepository.findById(oNo).get();
 	}
 
 	@Override
 	public Orders findOrderByOrderNoAndMemberNameandMemberPhoneNo(Long oNo, String MemberName, String memberPhoneNo) {
-		// 주어진 주문 번호 (oNo), 회원 이름 (MemberName), 회원 전화번호 (memberPhoneNo)를 사용하여 주문을 조회하려면 적절한 로직을 추가하세요.
-		// orderRepository를 사용하여 데이터베이스에서 주문을 조회할 수 있으며, 필요한 정보를 조건에 따라 조회할 수 있습니다.
+		// 주어진 주문 번호 (oNo), 회원 이름 (MemberName), 회원 전화번호 (memberPhoneNo)를 사용하여 주문을 조회하려면 적절한 로직을 추가
+		// orderRepository를 사용하여 데이터베이스에서 주문을 조회할 수 있으며, 필요한 정보를 조건에 따라 조회
 		return orderRepository.findOrderByOrderNoAndMemberNameAndMemberPhoneNo(oNo, MemberName, memberPhoneNo);
 	}
 	
