fc6704071cefbe524b8fe2bb9fb75ed92b606180
lsh96900410 <lsh96900410@gmail.com>
2023-10-18 14:32:49 +0900

    ... Cart Repository,Entity

---
+++ src/main/java/com/danaga/repository/CartRepository.java
@@ -0,0 +1,19 @@
+package com.danaga.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.danaga.entity.Cart;
+
+
+// 카트 전체보기 , 카트전체삭제, 카트개별삭제, 카트 추가, 카트업데이트 
+public interface CartRepository extends JpaRepository<Cart,Long> {
+	
+	// 유저 카트리스트
+	List<Cart> findCartByMemberId(String memberId);
+	
+	// 유저 카트 -> 제품 한개 [옵션 변경]
+	Cart findCartByOptionSetIdAndMemberId(String memberId,Long optionSetId);
+	
+}
