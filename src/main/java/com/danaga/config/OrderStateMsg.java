a1c83862a8adf8d5066b7843bb604606e08d97b4
kkr95101 <kkr95101@naver.com>
2023-10-19 19:56:25 +0900

    order change

--- src/main/java/com/danaga/config/OrderStateMsg.java
+++ src/main/java/com/danaga/config/OrderStateMsg.java
@@ -1,11 +1,6 @@
 package com.danaga.config;
 
-public class OrderStateMsg {
+public enum OrderStateMsg {
 
-	public static final String msg1 = "입금대기중";
-	public static final String msg2 = "배송중";
-	public static final String msg3 = "배송완료";
-	public static final String msg4 = "환불대기중";
-	public static final String msg5 = "환불완료";
-	public static final String msg6 = "취소";
+	입금대기중, 배송중, 배송완료, 환불대기중, 환불완료, 취소
 }
--- src/main/java/com/danaga/dao/OrderDaoImpl.java
+++ src/main/java/com/danaga/dao/OrderDaoImpl.java
@@ -25,91 +25,90 @@ import jakarta.persistence.criteria.Order;
 
 @Repository
 public class OrderDaoImpl implements OrderDao {
-	/*
-	 * @Autowired OrderRepository orderRepository;
-	 * 
-	 * @Autowired MemberRepository memberRepository;
-	 * 
-	 * @Autowired OrderItemRepository orderItemRepository;
-	 * 
-	 * @Override public Orders insert(Orders order) {
-	 * 
-	 * 
-	 * //OFindNo 월일+랜덤숫자5자리로 임의로 설정하기 LocalDateTime createdate =
-	 * order.getCreateDate(); int year = createdate.getYear(); // 년도 추출 int month =
-	 * createdate.getMonthValue(); // 월 추출 (1부터 12까지의 값) int day =
-	 * createdate.getDayOfMonth(); // 일 추출 (1부터 31까지의 값) String yearString =
-	 * Integer.toString(year); String subYearString = yearString.substring(2,4);
-	 * //년도 뒷자리2개만 추출 String monthString =Integer.toString(month); String dayString
-	 * =Integer.toString(day);
-	 * 
-	 * Random random = new Random(); // 5자리 난수 생성 int min = 10000; // 최소값 (5자리 숫자)
-	 * int max = 99999; // 최대값 (5자리 숫자) int randomNumber = random.nextInt(max - min
-	 * + 1) + min;
-	 * 
-	 * //년도(뒷두자리)+월+일+randomNumber(5자리)
-	 * order.setOrderNo(subYearString+monthString+dayString+randomNumber);
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 1.Order를 저장 2.OrderItemList를 저장 3.OrderItemList안의 Optionset저장 --service에서 처리
-	 * 4.Delivery저장 --service에서 처리 5.비회원 Member저장--service에서 처리
-	 * 
-	 * 
-	 * Orders insertOrder = orderRepository.save(order);
-	 * 
-	 * insertOrder.setOrderNo(insertOrder.getId());
-	 * 
-	 * List<OrderItem> itemList = insertOrder.getOrderItems();
-	 * 
-	 * for (OrderItem orderItem : itemList) { orderItem.setOrders(order);
-	 * orderItemRepository.save(orderItem); }
-	 * 
-	 * return insertOrder; // 저장된 주문 객체 반환 }
-	 * 
-	 * 
-	 * 
-	 * 
-	 * @Override public Orders updateOrdersByStatement(String oState) {
-	 * if(OrderStateMsg.msg1.equals(oState)) {
-	 * 
-	 * }else if (OrderStateMsg.msg2.equals(oState)) {
-	 * 
-	 * }else if (OrderStateMsg.msg3.equals(oState)) {
-	 * 
-	 * }else if (OrderStateMsg.msg4.equals(oState)) {
-	 * 
-	 * }else if (OrderStateMsg.msg5.equals(oState)) {
-	 * 
-	 * }else if (OrderStateMsg.msg6.equals(oState)) {
-	 * 
-	 * } return null; }
-	 * 
-	 * @Override public List<Orders> findOrdersByUserName(Long memberId) { // Member
-	 * findMember= memberRepository.findByMemberId(memberId); //memberRepository에
-	 * findbyMemberId 메서드 추가
-	 * 
-	 * // return orderRepository.findByMemberIdCode(findMember.getMemberIdCode);
-	 * 
-	 * return null; }
-	 * 
-	 * @Override public List<Orders> findOrdersWithOrderItemByMemberId(Long
-	 * memberId) { // 주어진 memberId를 사용하여 해당 회원의 주문 목록을 조회하고, 주문 항목을 함께 로드하려면 적절한 로직을
-	 * 추가 // orderRepository와 JOIN 또는 FetchType.LAZY를 사용하여 관련된 주문 항목을 함께 로드 return
-	 * null; }
-	 * 
-	 * @Override public Orders findOrdersByorderNo(Long oNo) { // 주어진 주문 번호 (oNo)를
-	 * 사용하여 해당 주문을 조회하려면 적절한 로직을 추가 // orderRepository를 사용하여 데이터베이스에서 주문을 조회 return
-	 * orderRepository.findById(oNo).get(); }
-	 * 
-	 * @Override public Orders findOrdersByOrderNoAndNameAndPhoneNo(String orderNo,
-	 * String userName,String phoneNo) { // 주어진 주문 번호 (oNo), 회원 이름 (MemberName), 회원
-	 * 전화번호 (memberPhoneNo)를 사용하여 주문을 조회하려면 적절한 로직을 추가 // orderRepository를 사용하여
-	 * 데이터베이스에서 주문을 조회할 수 있으며, 필요한 정보를 조건에 따라 조회 return
-	 * orderRepository.findOrdersByOrderNoAndMember_NameAndMember_PhoneNo(orderNo,
-	 * userName, phoneNo); }
-	 * 
-	 */
+	
+//	  @Autowired OrderRepository orderRepository;
+//	  
+//	  @Autowired MemberRepository memberRepository;
+//	  
+//	  @Autowired OrderItemRepository orderItemRepository;
+//	  
+//	  @Override public Orders insert(Orders order) {
+//	  
+//	/* //OFindNo 월일+랜덤숫자5자리로 임의로 설정하기 LocalDateTime createdate =
+//	 order.getCreateDate(); int year = createdate.getYear(); // 년도 추출 int month =
+//	 createdate.getMonthValue(); // 월 추출 (1부터 12까지의 값) int day =
+//	 createdate.getDayOfMonth(); // 일 추출 (1부터 31까지의 값) String yearString =
+//	 Integer.toString(year); String subYearString = yearString.substring(2,4);
+//	  //년도 뒷자리2개만 추출 String monthString =Integer.toString(month); String dayString
+//	  =Integer.toString(day);
+//	  
+//	  Random random = new Random(); // 5자리 난수 생성 int min = 10000; // 최소값 (5자리 숫자)
+//	  int max = 99999; // 최대값 (5자리 숫자) int randomNumber = random.nextInt(max - min
+//	  + 1) + min;
+//	  
+//	  //년도(뒷두자리)+월+일+randomNumber(5자리)
+//	  order.setOrderNo(subYearString+monthString+dayString+randomNumber);
+//	  
+//	  */
+//	  //1.Order를 저장 2.OrderItemList를 저장
+//	 // 4.Delivery저장 --service에서 처리 5.비회원 Member저장--service에서 처리
+//	  
+//	  Orders insertOrder = orderRepository.save(order);
+//	  
+//	  
+//	  List<OrderItem> itemList = insertOrder.getOrderItems();
+//	  
+//	  for (OrderItem orderItem : itemList) { 
+//		  orderItem.setOrders(order);
+//		  orderItemRepository.save(orderItem); 
+//	  }
+//	  
+//	  return insertOrder; // 저장된 주문 객체 반환 
+//	  
+//	  }
+//	  
+//	  @Override public Orders updateOrdersByStatement(String oState) {
+//	  /*
+//		if() {
+//		  
+//	  }else if () {
+//	  
+//	  }else if () {
+//	  
+//	  }else if () {
+//	  
+//	  }else if () {
+//	  
+//	  }else if () {
+//	  
+//	  } 
+//	  */
+//	  return null; 
+//	  
+//	  }
+//	  
+//	  @Override 
+//	  public List<Orders> findOrdersByMember_UserName(String userName) { 
+//		  
+//		  List<Orders> orders = orderRepository.findOrdersByMember_UserName(userName);
+//		  
+//		  return orders;
+//	  
+//	  }
+//	  
+//	  @Override 
+//	  public Orders findOrdersByorderNo(Long orderNo) { 
+//		  // 주어진 주문 번호 (oNo)를 사용하여 해당 주문을 조회하려면 적절한 로직을 추가 
+//		  // orderRepository를 사용하여 데이터베이스에서 주문을 조회
+//		  return orderRepository.findById(orderNo).get(); 
+//		  }
+//	  
+//	  @Override 
+//	  public Orders findOrdersByOrderNoAndNameAndPhoneNo(Long orderNo,String userName,String phoneNo) { 
+//		  // 주어진 주문 번호 (oNo), 회원 이름 (MemberName), 회원전화번호 (memberPhoneNo)를 사용하여 주문을 조회하려면 적절한 로직을 추가 
+//		  // orderRepository를 사용하여  데이터베이스에서 주문을 조회할 수 있으며, 필요한 정보를 조건에 따라 조회 
+//		  return orderRepository.findOrdersByOrderNoAndMember_NameAndMember_PhoneNo(orderNo,userName, phoneNo); 
+//		  
+//	  }
 
 }
