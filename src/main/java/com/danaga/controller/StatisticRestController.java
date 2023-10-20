1189b07db713536e171c93c359da363c2ab0c8cd
Roco-Lee <aowlrtmd@gmail.com>
2023-10-20 20:59:57 +0900

    admin bootstrap

--- src/main/java/com/danaga/controller/StatisticRestController.java
+++ src/main/java/com/danaga/controller/StatisticRestController.java
@@ -24,7 +24,7 @@ public class StatisticRestController {
 	@Operation(summary = "admin : 전체 통계 호출")
 	@GetMapping("/list")
 	public ResponseEntity<List<Statistic>> getStatisticListAll() {
-		List<Statistic> statistics = statisticService.Statistics();
+		List<Statistic> statistics = statisticService.thisMonthStatistic();
 		return ResponseEntity.status(HttpStatus.OK).body(statistics);
 	}
 	@Operation(summary = "admin : 전체 통계 호출")
