d872296c842b6f85f513783659dc31ccc1fdebe3
pawer <pawer@kimaaron>
2023-10-23 01:37:02 +0900

    board controller test T.T

--- src/main/java/com/danaga/repository/LikeConfigRepository.java
+++ src/main/java/com/danaga/repository/LikeConfigRepository.java
@@ -6,4 +6,5 @@ import com.danaga.entity.LikeConfig;
 
 public interface LikeConfigRepository extends JpaRepository<LikeConfig, Long>{
 
+	LikeConfig findByBoard_IdAndMember_id(Long boardId,Long MemberId);
 }
