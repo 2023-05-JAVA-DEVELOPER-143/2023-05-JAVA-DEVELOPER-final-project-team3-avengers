27749d99f3cf25f5e3543f890d8303e482815254
leedong617 <leedong617@gmail.com>
2023-10-26 16:34:10 +0900

    ..

--- src/main/java/com/danaga/controller/IndexController.java
+++ src/main/java/com/danaga/controller/IndexController.java
@@ -2,18 +2,14 @@ package com.danaga.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/index")
 public class IndexController {
 	
-	@GetMapping
-	public ModelAndView index() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("index");
-		return modelAndView;
+	@GetMapping("/index")
+	public String index() {
+		return "index";
 	}
 	
 	@GetMapping("/test")
