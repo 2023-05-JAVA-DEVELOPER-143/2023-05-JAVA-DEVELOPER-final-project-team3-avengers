28ebe51ed00044ddb2180f3e5fbb41eb8f0bd692
leedong617 <leedong617@gmail.com>
2023-10-27 14:35:02 +0900

    ..

--- src/main/java/com/danaga/controller/MemberController.java
+++ src/main/java/com/danaga/controller/MemberController.java
@@ -21,106 +21,111 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/member")
 public class MemberController {
-	
+
 	@Autowired
 	private MemberService memberService;
-	
+
 	@GetMapping("/member_login_form")
 	public String member_login_form() {
 		return "member/member_login_form";
 	}
+
 	@LoginCheck
 	@PostMapping("/member_login_action")
-	public String member_login_action(@ModelAttribute("fuser") MemberResponseDto member,Model model,HttpSession session) throws Exception {
+	public String member_login_action(@ModelAttribute("fuser") MemberResponseDto member, Model model, HttpSession session)
+			throws Exception {
 		String forwardPath = "";
 		try {
 			memberService.login(member.getUserName(), member.getPassword());
 			session.setAttribute("sUserId", member.getUserName());
-			forwardPath="redirect:../index";
-		}catch (MemberNotFoundException e) {
+			forwardPath = "redirect:../index";
+		} catch (MemberNotFoundException e) {
 			e.printStackTrace();
-			model.addAttribute("msg1",e.getMessage());
-			forwardPath="member/member_login_form";
-		}catch (PasswordMismatchException e) {
+			model.addAttribute("msg1", e.getMessage());
+			forwardPath = "member/member_login_form";
+		} catch (PasswordMismatchException e) {
 			e.printStackTrace();
-			model.addAttribute("msg2",e.getMessage());
-			forwardPath="member/member_login_form";
+			model.addAttribute("msg2", e.getMessage());
+			forwardPath = "member/member_login_form";
 		}
 		return forwardPath;
 	}
+
 	@GetMapping("/member_join_form")
 	public String member_join_form() {
 		return "member/member_join_form";
 	}
-	@PostMapping("member_join_action")
-	public String user_write_action(@ModelAttribute("fuser") MemberResponseDto member,Model model) throws Exception {
+
+	@PostMapping("/member_join_action")
+	public String user_write_action(@ModelAttribute("fuser") MemberResponseDto member, Model model) throws Exception {
 		String forward_path = "";
 		try {
 			memberService.joinMember(member);
-			forward_path="redirect:member_login_form";
-		}catch (ExistedMemberException e) {
+			forward_path = "redirect:member_login_form";
+		} catch (ExistedMemberException e) {
 			model.addAttribute("msg", e.getMessage());
 			model.addAttribute("fuser", member);
-			forward_path="member_join_form";
+			forward_path = "member_join_form";
 		}
 		return forward_path;
 	}
+
 	@GetMapping("/member_find_password_form")
 	public String member_findpassword_form() {
 		return "member/member_find_password_form";
 	}
+
 	@LoginCheck
 	@GetMapping("/member_info_form")
-	public String member_info_form(HttpServletRequest request,Model model) throws Exception {
+	public String member_info_form(HttpServletRequest request, Model model) throws Exception {
 		/************** login check **************/
 		/****************************************/
 		String loginUser = (String) request.getSession().getAttribute("sUserId");
-		model.addAttribute(memberService.findIdByUsername(loginUser));
+		MemberResponseDto member = memberService.getMemberBy(loginUser);
+		System.out.println(member);
+		model.addAttribute("loginUser", member);
 		return "member/member_info_form";
 	}
-	
+
 	@LoginCheck
-    @PostMapping("member_modify_action")
-    public String user_modify_action(@ModelAttribute MemberUpdateDto member,HttpServletRequest request) throws Exception {
-       /************** login check **************/
-       /****************************************/
-       
-       
-       memberService.updateMember(member);
-       return "redirect:member_info_form";
-    }
-	
+	@PostMapping("/member_modify_action")
+	public String user_modify_action(@ModelAttribute MemberUpdateDto member, HttpServletRequest request) throws Exception {
+		/************** login check **************/
+		/****************************************/
+		String sUserId = (String) request.getSession().getAttribute("sUserId");
+		Long sUserLongId = memberService.getMemberBy(sUserId).getId();
+		member.setId(sUserLongId);
+
+		memberService.updateMember(member);
+		return "redirect:member_info_form";
+	}
+
 	@LoginCheck
-	@GetMapping("member_logout_action")
+	@GetMapping("/member_logout_action")
 	public String user_logout_action(HttpServletRequest request) {
-		
+
 		/************** login check **************/
 		/****************************************/
 		request.getSession(false).invalidate();
-		
-		return "redirect:index";
+
+		return "redirect:../index";
 	}
-	
+
 	@LoginCheck
-	@PostMapping("member_remove_action")
+	@PostMapping("/member_remove_action")
 	public String member_remove_action(HttpServletRequest request) throws Exception {
 		/************** login check **************/
 		/****************************************/
-		String sUserId=(String)request.getSession().getAttribute("sUserId");
+		String sUserId = (String) request.getSession().getAttribute("sUserId");
 		memberService.deleteMember(sUserId);
 		request.getSession().invalidate();
-		return "redirect:index";
+		return "redirect:../index";
 	}
-	
-	@GetMapping({
-		"member_join_action",
-		"member_login_action",
-		"member_modify_action",
-		"member_remove_action"
-		})
+
+	@GetMapping({ "/member_join_action", "/member_login_action", "/member_modify_action", "/member_remove_action" })
 	public String user_get() {
-	String forwardPath = "redirect:index";
-	return forwardPath;
+		String forwardPath = "redirect:../index";
+		return forwardPath;
 	}
-	
+
 }
