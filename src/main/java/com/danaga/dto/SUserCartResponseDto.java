9339c0ba1a1cf49f00e954e42ae7993c05ed69ca
lsh96900410 <lsh96900410@gmail.com>
2023-10-27 19:48:54 +0900

    .카트

--- src/main/java/com/danaga/dto/SUserCartResponseDto.java
+++ src/main/java/com/danaga/dto/SUserCartResponseDto.java
@@ -18,15 +18,17 @@ import lombok.NoArgsConstructor;
 public class SUserCartResponseDto {
 	private Long id;
 	private Long memberId;
-	
+
 	private Integer qty;
 	private String productName;
 	private Integer totalPrice; // 토탈 금액
 	private String pImage; // 프로덕트 사진
 	private Long osId; // 옵션셋 아이디
+	private int 할인율;
 	private List<CartOptionDto> options = new ArrayList<>(); // 옵션 네임 밸류
 
 	public static SUserCartResponseDto toDto(Cart entity) {
+
 		return SUserCartResponseDto.builder().id(entity.getId()).memberId(entity.getMember().getId())
 				.qty(entity.getQty()).productName(entity.getOptionSet().getProduct().getName())
 				.totalPrice(entity.getOptionSet().getTotalPrice()).pImage(entity.getOptionSet().getProduct().getImg())
