d1bdda7fff2e896a999dccf9048f7f7c6a9b4c44
Roco-Lee <aowlrtmd@gmail.com>
2023-10-27 09:50:28 +0900

    admin Controller and page setting

--- src/main/java/com/danaga/service/StatisticServiceImpl.java
+++ src/main/java/com/danaga/service/StatisticServiceImpl.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.danaga.entity.Statistic;
