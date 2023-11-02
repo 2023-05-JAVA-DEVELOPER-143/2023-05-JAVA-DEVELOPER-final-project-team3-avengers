f7a387858ee0003de79d127c30fc8e51152a4a94
aaronkim <pawer77@gmail.com>
2023-11-02 18:34:43 +0900

    ..

---
+++ src/main/java/com/danaga/dto/BoardListPageDto.java
@@ -0,0 +1,10 @@
+package com.danaga.dto;
+
+import org.springframework.data.domain.Page;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
